da21d759f67a9edf8131e2b71dbedeb171d82d0a
GongT <admin@gongt.me>
2021-07-23 02:14:17 +0800

    fix rm error

--- build-in-container.sh
+++ build-in-container.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 podman pull docker.io/alpine:latest
 
 NAME="gongt-build-clash"
-podman rm -f "$NAME"
+podman rm -f --ignore "$NAME"
 podman run "--name=$NAME" \
 	"--volume=$SYSTEM_COMMON_CACHE/apk:/etc/apk/cache" \
 	"--volume=$SYSTEM_COMMON_CACHE/golang:/go/cache" \
